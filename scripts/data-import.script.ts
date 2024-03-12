3472bf09d31597a94ddcd4d140cdb1264a946324
GSPanue <gspanue@gmail.com>
2024-03-12 20:22:51 +0000

    Created dataset scripts with some todos

---
+++ scripts/data-import.script.ts
@@ -0,0 +1,5 @@
+/**
+ * @todo Import processed dataset from csv
+ * @todo Order data within dataset
+ * @todo Import dataset into MySQL database
+ */
---
+++ scripts/data-processing.script.ts
@@ -0,0 +1,6 @@
+/**
+ * @todo Import dataset from csv
+ * @todo Check each row for duplicates
+ * @todo Trim all strings
+ * @todo Create processed dataset and store in dataset/processed
+ */
