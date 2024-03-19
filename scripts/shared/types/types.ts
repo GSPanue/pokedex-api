de11d66d0b422de1f1af973f699125a7879ebace
GSPanue <gspanue@gmail.com>
2024-03-19 20:37:56 +0000

    Added File and RowStream types

--- scripts/shared/types/types.ts
+++ scripts/shared/types/types.ts
@@ -1,6 +1,33 @@
+import { EventEmitter } from 'events';
+
 interface Config {
   filename: string;
   path: string;
 }
 
-export { Config };
+interface File {
+  descriptor: {
+    path: string;
+    pathType: 'local' | 'remote';
+    name: string;
+    format: string;
+    mediatype?: string;
+  };
+  path: string;
+  stream(options?: { end?: number }): NodeJS.ReadableStream | null;
+  buffer: Promise<Buffer>;
+  rows(options?: {
+    keyed?: boolean;
+    sheet?: string;
+    size?: number;
+  }): NodeJS.ReadableStream | null;
+  addSchema(): Promise<void>;
+}
+
+interface RowStream extends EventEmitter {
+  on(event: 'data', listener: (row: any[]) => void): this;
+  on(event: 'end', listener: () => void): this;
+  on(event: 'error', listener: (error: Error) => void): this;
+}
+
+export { Config, File, RowStream };
