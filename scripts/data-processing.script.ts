83a1f1a16979785dcad0ce90a1275c6a84ea4c6e
GSPanue <gspanue@gmail.com>
2024-03-21 19:01:57 +0000

    Use papaparse to parse file

--- scripts/data-processing.script.ts
+++ scripts/data-processing.script.ts
@@ -1,46 +1,26 @@
 import * as path from 'path';
 
-import { getFile, getRows } from '@scripts/utils';
-import type { Config, File, RowStream } from '@scripts/shared/types';
+import { getFile, getData } from '@scripts/utils';
 
-const filename = 'pokedex_data_04_2021';
-const ext = '.csv';
+const filename: string = 'pokedex_data_04_2021';
+const ext: string = '.csv';
 
-const config: Config = {
+const config = {
   filename,
   path: path.join(__dirname, '../dataset/raw', `${filename}${ext}`),
 };
 
-type StartReturnType = void;
-interface Start {
-  (): StartReturnType;
-}
+const start = async () => {
+  const { path, filename } = config;
 
-const start: Start = async () => {
-  const file: File = getFile(config);
-  const rows: RowStream = await getRows(file);
+  try {
+    const file = await getFile(path);
+    const data = await getData(file);
 
-  const processedRows = [];
-
-  rows.on('data', (row) => {
-    /**
-     * @todo Trim string
-     * @todo Ensure data is correct data type
-     */
-    processedRows.push(row);
-  });
-
-  rows.on('end', () => {
-    const length = processedRows.length;
-
-    console.log(`Processed ${length} rows successfully`);
-    process.exit(0);
-  });
-
-  rows.on('error', (error) => {
-    console.error(`Error reading rows: ${error}`);
-    process.exit(1);
-  });
+    console.log(data);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 start();
--- scripts/utils/dataset/dataset.utils.ts
+++ scripts/utils/dataset/dataset.utils.ts
@@ -1,40 +1,83 @@
-import { open } from 'data.js';
+import { promises as fs } from 'fs';
+import { Promise as QPromise } from 'q';
+import { parse } from 'papaparse';
+import { pick } from 'lodash';
 
-import type { Config, File, RowStream } from '@scripts/shared/types';
-
-type GetFileReturnType = File;
+type GetFileReturnType = Promise<string>;
 interface GetFile {
-  (config: Config): GetFileReturnType;
+  (path: string): GetFileReturnType;
 }
 
-const getFile: GetFile = (config) => {
-  const { path } = config;
-
+const getFile: GetFile = async (path) => {
   try {
-    const file = open(path);
+    const file = await fs.readFile(path, 'utf8');
 
     return file;
   } catch (error) {
-    console.log(`Error opening file: ${error}`);
-    process.exit(1);
+    throw new Error(`Error retrieving file: ${error}`);
   }
 };
 
-type GetRowsReturnType = Promise<RowStream>;
-interface GetRows {
-  (file: File): GetRowsReturnType;
+type PokemonData = {
+  pokedex_number: number | null;
+  name: string | null;
+  german_name: string | null;
+  japanese_name: string | null;
+  generation: number | null;
+  status: string | null;
+  species: string | null;
+  type_1: string | null;
+  type_2: string | null;
+  height_m: number | null;
+  weight_kg: number | null;
+  ability_1: string | null;
+  ability_2: string | null;
+  ability_hidden: string | null;
+};
+type GetDataReturnType = Promise<PokemonData[]>;
+interface GetData {
+  (file: string): GetDataReturnType;
 }
 
-const getRows: GetRows = async (file) => {
+const getData: GetData = async (file) => {
   try {
-    const rows: RowStream = file.rows();
+    const results = [];
 
-    return rows;
+    const handleStep = ({ data }) => {
+      const keys = [
+        'pokedex_number',
+        'name',
+        'german_name',
+        'japanese_name',
+        'generation',
+        'status',
+        'species',
+        'type_1',
+        'type_2',
+        'height_m',
+        'weight_kg',
+        'ability_1',
+        'ability_2',
+        'ability_hidden',
+      ];
+
+      results.push(pick(data, keys));
+    };
+
+    await QPromise((resolve, reject) => {
+      parse(file, {
+        header: true,
+        dynamicTyping: true,
+        step: handleStep,
+        complete: resolve,
+        error: reject,
+      });
+    });
+
+    return results;
   } catch (error) {
-    console.error(`Error getting rows: ${error}`);
-    process.exit(1);
+    throw new Error(`Error processing data: ${error}`);
   }
 };
 
-export type { RowStream, GetRowsReturnType };
-export { getFile, getRows };
+export { getFile, getData };
