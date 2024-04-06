541a83e2b61ed66db5871b233cc30220302119db
GSPanue <gspanue@gmail.com>
2024-04-06 19:32:04 +0100

    Moved PokemonData to shared types

--- scripts/shared/types/types.ts
+++ scripts/shared/types/types.ts
@@ -1,11 +1,30 @@
+type PokemonData = {
+  pokedex_number: number | null;
+  name: string | null;
+  german_name: string | null;
+  japanese_name: string | null;
+  generation: number | null;
+  status: string | null;
+  species: string | null;
+  type_1: string | null;
+  type_2: string | null;
+  height_m: number | null;
+  weight_kg: number | null;
+  ability_1: string | null;
+  ability_2: string | null;
+  ability_hidden: string | null;
+};
+
+type PokemonDataKeys = keyof PokemonData;
+
 type Config = {
   filename: string;
   keys: {
-    requiredKeys: string[];
-    selectedKeys: string[];
+    requiredKeys: Array<PokemonDataKeys>;
+    selectedKeys: Array<PokemonDataKeys>;
   };
   inputPath: string;
   outputPath: string;
 };
 
-export { Config };
+export { Config, PokemonData };
