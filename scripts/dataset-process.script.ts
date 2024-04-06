4d8c4d2779cb25d3b9e0bf5cd27f24fea73a199b
GSPanue <gspanue@gmail.com>
2024-04-06 20:40:06 +0100

    import getFile from shared utils

--- scripts/dataset-process.script.ts
+++ scripts/dataset-process.script.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
-import { getFile, getData, processData, exportData } from '@scripts/utils';
+import { getData, processData, exportData } from '@scripts/utils';
+import { getFile } from '@scripts/shared';
 import type { Config } from '@scripts/shared';
 
 const filename: string = 'pokedex_data_04_2021.csv';
