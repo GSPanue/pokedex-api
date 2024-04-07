ec0f8c3de4996b9e0239035363807a8657bbe9a5
GSPanue <gspanue@gmail.com>
2024-04-07 17:37:42 +0100

    Get data from processed dataset

--- scripts/dataset-import.script.ts
+++ scripts/dataset-import.script.ts
@@ -1,5 +1,44 @@
-/**
- * @todo Import processed dataset from csv
- * @todo Order data within dataset
- * @todo Import dataset into MySQL database
- */
+import { join } from 'path';
+
+import { getFile, getData } from '@scripts/shared';
+import type { Config } from '@scripts/shared';
+
+const filename: string = 'pokedex_data_04_2021.csv';
+
+const config: Partial<Config> = {
+  keys: {
+    selectedKeys: [
+      'pokedex_number',
+      'name',
+      'german_name',
+      'japanese_name',
+      'generation',
+      'status',
+      'species',
+      'type_1',
+      'type_2',
+      'height_m',
+      'weight_kg',
+      'ability_1',
+      'ability_2',
+      'ability_hidden',
+    ],
+  },
+  inputPath: join(__dirname, '../dataset/processed', filename),
+};
+
+const start = async () => {
+  const { inputPath, keys } = config;
+
+  console.log('Reading file...\n');
+  const file = await getFile(inputPath);
+
+  console.log('Retrieving data from file...\n');
+  const data = await getData(file, keys);
+
+  // Store data in a MySQL database
+
+  console.log(data);
+};
+
+start();
