afa40887f8b2fb526850973fb1209dce25d9ea1f
GSPanue <gspanue@gmail.com>
2024-04-09 17:46:25 +0100

    Exported entities

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,4 +1,32 @@
 import { Module } from '@nestjs/common';
+import {
+  Pokemon,
+  EnglishName,
+  JapaneseName,
+  GermanName,
+  Types,
+  Generations,
+  Height,
+  Rarity,
+  Weight,
+  Species,
+  Abilities,
+} from '@shared/entities';
 
-@Module({})
+@Module({
+  providers: [],
+  exports: [
+    Pokemon,
+    EnglishName,
+    JapaneseName,
+    GermanName,
+    Types,
+    Generations,
+    Height,
+    Rarity,
+    Weight,
+    Species,
+    Abilities,
+  ],
+})
 export class SharedModule {}
