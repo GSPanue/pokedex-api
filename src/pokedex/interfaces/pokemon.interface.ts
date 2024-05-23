e9da7a9ebef83e8caec22a38372cf87aa00b7e82
GSPanue <gspanue@gmail.com>
2024-05-23 18:52:13 +0100

    Created a pokemon interface

---
+++ src/pokedex/interfaces/pokemon.interface.ts
@@ -0,0 +1,26 @@
+export interface Pokemon {
+  id: number;
+  name: string;
+  german_name: string;
+  japanese_name: string;
+  generation: number;
+  rarity: string;
+  species: string;
+  abilities: {
+    ability_1: string | null;
+    ability_2: string | null;
+    ability_hidden: string | null;
+  };
+  types: {
+    type_1: string;
+    type_2: string | null;
+  };
+  height: {
+    value: number;
+    unit: string;
+  };
+  weight: {
+    value: number;
+    unit: string;
+  };
+}
