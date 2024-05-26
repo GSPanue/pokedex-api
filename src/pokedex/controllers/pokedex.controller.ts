7024cc69ced7289286a92420642aabb15f7ed3b9
GSPanue <gspanue@gmail.com>
2024-05-26 16:57:28 +0100

    Set types and used interfaces

--- src/pokedex/controllers/pokedex.controller.ts
+++ src/pokedex/controllers/pokedex.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get, Query, Param } from '@nestjs/common';
-import { PokedexService } from '../providers';
+import { IPokedexService, IPokemon } from '../interfaces';
 
 @Controller()
 export class PokedexController {
-  constructor(private pokedex: PokedexService) {}
+  constructor(private pokedex: IPokedexService) {}
 
   @Get('pokedex')
-  getPokemon(
+  async getPokemon(
     @Query('limit') limit: number = 10,
     @Query('offset') offset: number = 0,
     @Query('sort') sort: string = '',
     @Query('order') order: string = 'asc',
-  ) {
+  ): Promise<IPokemon[]> {
     /**
      * @todo Find all Pokémon
      */
@@ -20,7 +20,7 @@ export class PokedexController {
   }
 
   @Get('pokedex/:id')
-  getPokemonById(@Param('id') id: number) {
+  async getPokemonById(@Param('id') id: number): Promise<IPokemon[]> {
     /**
      * @todo Find Pokémon by ID
      */
