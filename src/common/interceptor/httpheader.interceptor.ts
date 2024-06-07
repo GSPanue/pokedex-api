089f82f640a5e5ae8d46d9fbd83b4202059b5856
GSPanue <gspanue@gmail.com>
2024-06-07 20:04:07 +0100

    Added etag logic

--- src/common/interceptor/httpheader.interceptor.ts
+++ src/common/interceptor/httpheader.interceptor.ts
@@ -4,7 +4,8 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { tap } from 'rxjs';
+import { map } from 'rxjs/operators';
+import * as ETag from 'etag';
 
 import type { Observable } from 'rxjs';
 
@@ -15,16 +16,28 @@ export class HttpHeaderInterceptor implements NestInterceptor {
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
-      tap(() => {
+      map((data) => {
         const ctx = context.switchToHttp();
+        const req = ctx.getRequest();
         const res = ctx.getResponse();
 
         const isOK = res.statusCode >= 200 && res.statusCode < 300;
 
         if (isOK) {
+          const etag = ETag(JSON.stringify(data));
+          const doesMatchETag = req.headers['if-none-match'] === etag;
+
+          res.setHeader('ETag', etag);
+
+          if (doesMatchETag) {
+            res.status(304);
+
+            return;
+          }
           /**
-           * @todo Set headers
+           * @todo Set custom headers
            */
+          return data;
         }
       }),
     );
