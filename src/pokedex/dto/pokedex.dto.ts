928a31cba85893f4bfd9e0847955c9cd8c56e5af
GSPanue <gspanue@gmail.com>
2024-06-16 17:23:48 +0100

    Created pokedex dtos

--- src/pokedex/dto/pokedex.dto.ts
+++ src/pokedex/dto/pokedex.dto.ts
@@ -1,27 +1,35 @@
-import { IsOptional, IsNumber, Min, Max, IsString } from 'class-validator';
+import {
+  IsOptional,
+  IsNumber,
+  Min,
+  Max,
+  IsString,
+  IsIn,
+} from 'class-validator';
 
 export class GetPokemonDto {
   @IsOptional()
   @IsNumber()
   @Min(1)
   @Max(100)
-  public limit?: number;
+  public limit?: number = 10;
 
   @IsOptional()
   @IsNumber()
   @Min(0)
-  public offset?: number;
+  public offset?: number = 0;
 
   @IsOptional()
   @IsString()
   public sort?: string;
 
   @IsOptional()
-  @IsString()
+  @IsIn(['asc', 'desc'])
   public order?: string;
 }
 
 export class GetPokemonByIdDto {
   @IsNumber()
+  @Min(1)
   public id: number;
 }
