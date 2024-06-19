7ca8f136c0d22ac84cc67b782a892d39f907a47d
GSPanue <gspanue@gmail.com>
2024-06-19 17:27:53 +0100

    Inject pokemon repo

--- src/pokedex/providers/pokedex.service.ts
+++ src/pokedex/providers/pokedex.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+import { Pokemon } from '@src/entity';
 import { IPokedexService, IPokemon } from '../interfaces';
 
 import type { GetPokemonDto, GetPokemonByIdDto } from '../dto';
 
 @Injectable()
 export class PokedexService implements IPokedexService {
+  constructor(
+    @InjectRepository(Pokemon)
+    private pokemonRepository: Repository<Pokemon>,
+  ) {}
+
   getPokemon(query: GetPokemonDto): IPokemon[] {
     return [];
   }
