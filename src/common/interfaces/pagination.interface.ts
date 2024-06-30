0b79e476bda3ac9ce6f891d5badc97d4cf13d50a
GSPanue <gspanue@gmail.com>
2024-06-30 17:05:21 +0100

    Created pagination utils

--- src/common/interfaces/pagination.interface.ts
+++ src/common/interfaces/pagination.interface.ts
@@ -2,6 +2,30 @@ export interface ICalculateSkip {
   (limit: number, offset: number): number;
 }
 
-export interface ICalculateTotalPages {
+export interface ICalculatePageCount {
   (limit: number, offset: number): number;
+}
+
+export interface ICalculateItemCount {
+  (results): number;
+}
+
+export interface ICalculatePageCount {
+  (limit: number, count: number): number;
+}
+
+export interface ICalculateCurrentPage {
+  (offset: number): number;
+}
+
+export interface ICalculatePageSize {
+  (limit: number): number;
+}
+
+export interface IHasNextPage {
+  (offset: number, pageCount: number): boolean;
+}
+
+export interface IHasPreviousPage {
+  (offset: number, pageCount: number): boolean;
 }
--- src/common/utils/pagination.utils.ts
+++ src/common/utils/pagination.utils.ts
@@ -1,6 +1,26 @@
-import type { ICalculateSkip, ICalculateTotalPages } from '@common';
+import type {
+  ICalculateSkip,
+  ICalculateItemCount,
+  ICalculatePageCount,
+  ICalculateCurrentPage,
+  ICalculatePageSize,
+  IHasNextPage,
+  IHasPreviousPage,
+} from '@common';
 
 export const calculateSkip: ICalculateSkip = (limit, offset) => limit * offset;
 
-export const calculateTotalPages: ICalculateTotalPages = (limit, count) =>
+export const calculateItemCount: ICalculateItemCount = (results) =>
+  results.length;
+
+export const calculatePageCount: ICalculatePageCount = (limit, count) =>
   Math.ceil(count / limit);
+
+export const calculateCurrentPage: ICalculateCurrentPage = (offset) => offset;
+
+export const calculatePageSize: ICalculatePageSize = (limit) => limit;
+
+export const hasNextPage: IHasNextPage = (offset, pageCount) =>
+  offset < pageCount - 1;
+
+export const hasPreviousPage: IHasPreviousPage = (offset) => offset > 0;
