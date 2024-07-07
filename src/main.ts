1d304fd656ac789cf13892f2fc7fbecac3718410
GSPanue <gspanue@gmail.com>
2024-07-07 17:49:04 +0100

    Set global prefix to `api/v1`

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const instance = app.getHttpAdapter().getInstance();
 
+  app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
