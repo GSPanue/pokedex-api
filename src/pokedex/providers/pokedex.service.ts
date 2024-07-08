6de07bbe4201cf5d1d596af33b2db5bca3382695
GSPanue <gspanue@gmail.com>
2024-07-08 20:28:01 +0100

    Throw 404 when pokemon are not found by id

--- src/pokedex/providers/pokedex.service.ts
+++ src/pokedex/providers/pokedex.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { isEmpty } from 'lodash';
 
 import { Pokemon } from '@entities';
 import { calculateSkip } from '@common';
@@ -52,6 +53,12 @@ export class PokedexService implements IPokedexService {
     });
 
     const transformedResults: IPokemon[] = transformToPokemonArray(results);
+
+    const hasNoResults = isEmpty(results);
+
+    if (hasNoResults) {
+      throw new NotFoundException(`Not Found`);
+    }
 
     return {
       results: transformedResults,
