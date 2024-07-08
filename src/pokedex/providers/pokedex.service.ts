4119c0f7a8666318444b33eb3699cec9af415277
GSPanue <gspanue@gmail.com>
2024-07-08 20:33:11 +0100

    Small refactor; add comments

--- src/pokedex/providers/pokedex.service.ts
+++ src/pokedex/providers/pokedex.service.ts
@@ -24,14 +24,17 @@ export class PokedexService implements IPokedexService {
   async getPokemon(query: GetPokemonDto): Promise<IPokedexResponse> {
     const { limit, offset, sort, order } = query;
 
+    // Get the number of items to skip
     const skip = calculateSkip(limit, offset);
 
+    // Fetch results
     const [results, count] = await this.pokemonRepository.findAndCount({
       skip,
       take: limit,
       order: createOrderObject(sort, order),
     });
 
+    // Transform results
     const transformedResults: IPokemon[] = transformToPokemonArray(results);
 
     return {
@@ -43,6 +46,7 @@ export class PokedexService implements IPokedexService {
   async getPokemonById(params: GetPokemonByIdDto): Promise<IPokedexResponse> {
     const { id } = params;
 
+    // Fetch results
     const [results, count] = await this.pokemonRepository.findAndCount({
       where: {
         pokedex_id: id,
@@ -52,13 +56,13 @@ export class PokedexService implements IPokedexService {
       },
     });
 
+    // Transform results
     const transformedResults: IPokemon[] = transformToPokemonArray(results);
 
+    // Check if results are empty, throw an error if so
     const hasNoResults = isEmpty(results);
 
-    if (hasNoResults) {
-      throw new NotFoundException(`Not Found`);
-    }
+    if (hasNoResults) throw new NotFoundException(`Not Found`);
 
     return {
       results: transformedResults,
