9da6bb4504f6320fa3a4a6ad98f84c4410cff375
GSPanue <gspanue@gmail.com>
2024-08-25 20:18:55 +0100

    Generate initial schema if no migrations are found

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 FLAG_FILE="/server_data/.initialized"
+MIGRATIONS_DIR="/server/src/migrations"
 
 if [ ! -f "$FLAG_FILE" ]; then
   echo "First time setup..."
 
   # Install project dependencies
   npm install
+
+  if [ -z "$(ls -A $MIGRATIONS_DIR)" ]; then
+    echo "No migrations found. Generating initial schema..."
+    npm run migration:generate /server/src/migrations/InitialSchema
+  fi
 
   # Process and import dataset into the database
   npm run data:process
