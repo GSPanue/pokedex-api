94de8d8bca33789582f237f52e83acecdf56da9f
GSPanue <gspanue@gmail.com>
2024-09-15 00:59:49 +0100

    Implemented caching with Redis

--- src/pokedex/interceptors/cache.interceptor.ts
+++ src/pokedex/interceptors/cache.interceptor.ts
@@ -7,33 +7,86 @@ import {
 } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+import { defaultQuery } from '@common';
 import { createCacheKey } from '../utils';
 
+import type { Pokemon } from '@entities';
+import type {
+  ICacheInterceptor,
+  IPokemon,
+  IPokedexResponse,
+} from '../interfaces';
+
 @Injectable()
-export class CacheInterceptor implements NestInterceptor {
+export class CacheInterceptor implements ICacheInterceptor, NestInterceptor {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  async getPokemonById(id) {
-    let cacheKey = createCacheKey(`id:${id}`);
+  async cachePokemonQuery(key: string, data: Pokemon[]): Promise<void> {
+    const cacheKey = createCacheKey(key);
 
-    // Get data cache keys
-    const dataCacheKeys: any = await this.cacheManager.get(cacheKey);
+    // Create data cache keys
+    const dataCacheKeys: string[] = data.map((datum) =>
+      createCacheKey(`data:${datum.id}`),
+    );
 
-    const cachedResults = [];
+    // Store query as key and ids as value
+    await this.cacheManager.set(cacheKey, dataCacheKeys);
+  }
 
-    if (dataCacheKeys) {
-      // Get Pokemon data from cache using keys
-      for (const key of dataCacheKeys) {
-        cacheKey = createCacheKey(`data:${key}`);
+  async cachePokemonData(id: number, data: IPokemon): Promise<void> {
+    const cacheKey = createCacheKey(`data:${id}`);
 
-        const cachedResult = await this.cacheManager.get(cacheKey);
+    // Get data from cache
+    const cachedResult = await this.cacheManager.get<IPokemon | undefined>(
+      cacheKey,
+    );
 
-        cachedResults.push(cachedResult);
-      }
+    // Check if no data exists in cache
+    if (!cachedResult) {
+      // Store data
+      await this.cacheManager.set(cacheKey, data);
     }
+  }
+
+  async retrievePokemon(key: string): Promise<IPokemon[]> {
+    // Get data cache keys from query
+    const dataCacheKeys = await this.retrievePokemonQuery(key);
+
+    const hasDataCacheKeys = dataCacheKeys.length > 0;
+
+    // Check for data cache keys
+    if (hasDataCacheKeys) {
+      // Return data
+      return await this.retrievePokemonData(dataCacheKeys);
+    }
+
+    return [];
+  }
+
+  async retrievePokemonQuery(key: string): Promise<string[]> {
+    const cacheKey = createCacheKey(key);
+
+    // Get data cache keys assigned to query from cache
+    const cachedResults = await this.cacheManager.get<string[] | undefined>(
+      cacheKey,
+    );
+
+    // Return data cache keys
+    if (cachedResults) return cachedResults;
+
+    return [];
+  }
+
+  async retrievePokemonData(keys: string[]): Promise<IPokemon[]> {
+    // Retrieve full data from cache using data cache keys
+    const cachedResults: IPokemon[] = await Promise.all(
+      keys.map(async (key) => {
+        return this.cacheManager.get<IPokemon>(key);
+      }),
+    );
 
     return cachedResults;
   }
@@ -47,26 +100,82 @@ export class CacheInterceptor implements NestInterceptor {
 
     const path = req.route.path;
 
-    const { params } = req;
+    const { query, params } = req;
 
     const isPokedexResource = path.endsWith('/pokedex');
     const isPokedexWithIdResource = path.endsWith('/pokedex/:id');
 
-    return next.handle().pipe(
-      tap(async (response) => {
-        const { rawResults, results } = response;
+    let cachedResults = [];
 
+    // Check cache
+    if (isPokedexResource) {
+      const {
+        limit: defaultLimit,
+        offset: defaultOffset,
+        sort: defaultSort,
+        order: defaultOrder,
+      } = defaultQuery;
+
+      const {
+        limit = defaultLimit,
+        offset = defaultOffset,
+        sort = defaultSort,
+        order = defaultOrder,
+      } = query;
+
+      cachedResults = await this.retrievePokemon(
+        `limit:${limit}:offset:${offset}:sort:${sort}:order:${order}`,
+      );
+    } else if (isPokedexWithIdResource) {
+      const { id } = params;
+
+      cachedResults = await this.retrievePokemon(`id:${id}`);
+    }
+
+    // Return cached data
+    if (cachedResults.length > 0) {
+      return of({
+        results: cachedResults,
+        // @todo Cache count for /pokedex resource for custom headers
+        count: cachedResults.length,
+        ...(isPokedexResource && {
+          query: {
+            ...defaultQuery,
+            ...query,
+          },
+        }),
+      });
+    }
+
+    // Otherwise, cache data
+    return next.handle().pipe(
+      tap(async (response: IPokedexResponse) => {
+        const { query, rawResults, results } = response;
+
+        let cacheKey: string = '';
+
+        // Set query as cache key
         if (isPokedexResource) {
-          // Cache results
+          const { limit, offset, sort, order } = query;
+
+          cacheKey = `limit:${limit}:offset:${offset}:sort:${sort}:order:${order}`;
         } else if (isPokedexWithIdResource) {
           const { id } = params;
 
-          const cachedResult = await this.getPokemonById(id);
-
-          if (cachedResult.length === 0) {
-            // So store them then
-          }
+          cacheKey = `id:${id}`;
         }
+
+        // Cache key and the pokemon ids as the value
+        if (cacheKey) await this.cachePokemonQuery(cacheKey, rawResults);
+
+        // Cache full Pokemon data
+        await Promise.all(
+          rawResults.map((rawResult, i) => {
+            const { id } = rawResult;
+
+            return this.cachePokemonData(id, results[i]);
+          }),
+        );
       }),
     );
   }
--- src/pokedex/interfaces/cache.interface.ts
+++ src/pokedex/interfaces/cache.interface.ts
@@ -1,3 +1,26 @@
+import type { ExecutionContext, CallHandler } from '@nestjs/common';
+import type { Observable } from 'rxjs';
+
+import type { Pokemon } from '@entities';
+import type { IPokemon } from '.';
+
+export interface ICacheInterceptor {
+  cachePokemonQuery(key: string, data: Pokemon[]): Promise<void>;
+
+  cachePokemonData(id: number, data: IPokemon): Promise<void>;
+
+  retrievePokemon(key: string): Promise<IPokemon[]>;
+
+  retrievePokemonQuery(key: string): Promise<string[]>;
+
+  retrievePokemonData(keys: string[]): Promise<IPokemon[]>;
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>>;
+}
+
 export interface ICreateCacheKey {
   (value: string): string;
 }
